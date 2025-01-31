7b00817a372ae246d4c30f22d598455a881089b8
Renuka-Bandaru <renuka.bandaru777@gmail.com>
2025-01-31 10:53:29 +0530

    basic-movie-booking

---
+++ movie.js
@@ -0,0 +1,19 @@
+const cinema = {
+    name: "AMB cinemas",
+    location: "Hitech-city",
+    movies: [
+        {
+            title: "KALKI",
+            shows: ["10:00AM","3:00PM"],
+            seats: [60,60],
+            image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC45LzEwICA3OS42SyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00418119-urxwxzudkv-portrait.jpg"
+        },
+        {
+            title: "Pushpa 2",
+            shows: ["11:00AM","2:00PM"],
+            seats: [60,60],
+            image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-MS44SyBMaWtlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00430933-fdjcheqsra-portrait.jpg"
+        }
+    ]
+};
+
